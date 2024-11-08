ac57f10031779a4b35d4155e9724021ea6152b2c
Chris <taiwoakinfenwa2019@gmail.com>
2024-11-08 14:34:48 +0100

    feat: update dashboard

--- src/components/HomeNav.jsx
+++ src/components/HomeNav.jsx
@@ -14,23 +14,23 @@ export default function HomeNav() {
         height={20}
       />
       <div className="hidden gap-9 md:flex lg:pr-28">
-        <Link href='/home' className="text-lg font-medium text-white san">
+        <Link href="/home" className="text-lg font-medium text-white san">
           Home
         </Link>
         <Link href="/pricing" className="text-lg font-medium text-white san">
           Pricing
         </Link>
-        <Link href='/blog' className="text-lg font-medium text-white san">
+        <Link href="/blog" className="text-lg font-medium text-white san">
           Blog
         </Link>
-        <Link href='/contact' className="text-lg font-medium text-white san ">
+        <Link href="/contact" className="text-lg font-medium text-white san ">
           Contact
         </Link>
       </div>
       <button className="border-[0.5px] hidden md:flex   rounded-[5px]  px-10 py-4 text-lg font-medium ">
         Login
       </button>
-      <IoMenu className="text-[#e7492b] bg-white flex md:hidden border-[0.5px] rounded-[7px] py-2 w-[80px] h-[60px]  font-medium" />
+      <IoMenu className="text-[#e7492b] bg-white flex md:hidden border-[0.5px] rounded-[7px] py-2 w-[80px] h-[40px]  font-medium" />
     </div>
   );
 }
